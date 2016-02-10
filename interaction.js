2cd3d6dcdd106888ef7ee5514fb4b4a09b593a93
gahiguy <gahiguyar@gmail.com>
2016-02-10 10:12:56 +0100

    Q5

--- interaction.js
+++ interaction.js
@@ -3,10 +3,61 @@
 // L'interacteur viendra dans un second temps donc ne vous en souciez pas au départ.
 function DnD(canvas, interactor) {
 	// Définir ici les attributs de la 'classe'
+	this.posInitX = 0;
+	this.posInitY = 0;
+	this.posFinX = 0;
+	this.posFinY = 0;
+	this.pression = false;
+	
+	
+	this.debug  = function (eventText) {
+		console.log(eventText, this.posInit1, this.posInit2, this.posFin1, this.posFin2, this.pression);
+	}.bind(this);
 
 	// Developper les 3 fonctions gérant les événements
-
+	//fonction click
+	this.maFctGérantLaPression = function(evt){
+		var res = getMousePosition(canvas, evt) ;
+		//recuperer les coordonnées du curseur
+		this.posInitX = res.x;
+		this.posInitY = res.y;
+		//je suis en mode click
+		this.pression = true;
+		//affiche les coordonnées et le mode 
+		this.debug("mouse down");
+		
+	}.bind(this)
+	//fonctiondeplacement
+	this.maFctGérantLeDéplacement = function(evt){
+		if(this.pression){
+			var res = getMousePosition(canvas, evt) ;
+			//recuperer les coordonnées du curseur
+			this.posFin1 = res.x;
+			this.posFin2 = res.y;
+		
+			//affiche les coordonnées 
+		this.debug("mouse move");
+		}
+		
+	}.bind(this)
+	//fonctionRelachement
+	this.maFctGérantLeRelâchement= function(evt){
+		
+		var res = getMousePosition(canvas, evt) ;
+		
+		
+		//affiche les coordonnées et le mode 
+		this.debug("mouse up");
+		
+		this.pression = false;
+		
+		
+		
+	}.bind(this);
 	// Associer les fonctions précédentes aux évènements du canvas.
+	canvas.addEventListener('mousedown', this.maFctGérantLaPression, false);
+    canvas.addEventListener('mousemove', this.maFctGérantLeDéplacement, false);
+    canvas.addEventListener('mouseup', this.maFctGérantLeRelâchement, false);
 };
 
 
